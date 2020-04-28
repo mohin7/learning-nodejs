414f893a5eef58e9942afa36a928265ab95094ac
MD Mohin <csemohin@gmail.com>
2020-04-28 21:38:59 +0600

    learn nodejs module system
    Signed-off-by: MD Mohin <csemohin@gmail.com>

--- 02-module-system/path.js
+++ 02-module-system/path.js
@@ -10,10 +10,12 @@ const myCurrentDirectory = __dirname;
 console.log(path.basename(myCurrentDirectory)); //out will be show nodejs
 console.log(path.basename(myCurrentPath)); //out will be show path.js
 console.log(path.extname(myCurrentPath)); //out will be show .js
+console.log(path.parse(myCurrentPath)); // return {root: '/',dir: '/mnt/c/Users/MOHIN/desktop/nodejs',base: 'path.js',ext: '.js',name: 'path'}
 
 console.log(path.isAbsolute(myCurrentPath)); // return true
-console.log(path.isAbsolute('./math.js')); // return false
-
+console.log(path.isAbsolute("./math.js")); // return false
+console.log(path.join("user", "local", "mohin", "main.js")); // return user/local/mohin/main.js
+console.log(path.resolve(__dirname, "mohin", "main.js")); // return mnt/c/Users/MOHIN/desktop/nodejs/mohin/main.js
 
 //we create a path object
 const pathObj = {
