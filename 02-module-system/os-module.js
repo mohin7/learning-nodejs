31b0c7f35c31c5fa7eea15c1f502e80617f542bb
MD Mohin <csemohin@gmail.com>
2020-05-01 16:53:56 +0600

    os module
    Signed-off-by: MD Mohin <csemohin@gmail.com>

---
+++ 02-module-system/os-module.js
@@ -0,0 +1,19 @@
+const os = require('os');
+
+// console.log(os.arch()); // return x64
+// console.log(os.cpus()); // get cpu information like model, speed, times
+// console.log(os.freemem()); // return free memory space
+// console.log(os.hostname()); // return DESKTOP-LN6NN4B
+// console.log(os.homedir()); // return home dir "/home/mohin"
+// console.log(os.type()); // return os type "Linux"
+// console.log(os.userInfo()); // return user info object 
+// console.log(os.uptime()); // return how log running
+// console.log(os.networkInterfaces()); // return network information
+
+
+
+
+
+
+
+
